cd28d70c4ea07bc0e523e9cf92ed42cdd7254adb
Wassim Chegham <github@wassimchegham.com>
2016-08-09 22:38:55 +0200

    🐹

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('Component: App', () => {
+  it('should create an instance', () => {
+    let component = new AppComponent();
+    expect(component).toBeTruthy();
+  });
+});
